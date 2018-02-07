76d544155d609c5be0f220c5db9c2541596d33ff
joemphilips <joemphilips@gmail.com>
2018-02-07 23:02:54 +0900

    make sure it is able to instantiate psbtSerializer

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const bitcoin = require("bitcoinjs-lib");
+
+export default class psbtSerializer {
+  constructor(public hex: string) {
+  }
+
+  public deserialize () {
+
+  }
+}
--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,11 @@
-import * as ava from 'ava'
-const test = ava.test
+import { test, TestContext } from 'ava'
+import psbtSerializer from '../src'
 const tv: any = require('./fixtures/psbt.json')
 
-test('valid test vector', (t: ava.TestContext):void => {
-  t.deepEqual([1,2], [1,2])
+test('valid test vector', (t: TestContext):void => {
+  t.plan(4)
+  tv.invalid.forEach((testcase: any) => {
+    const ser: psbtSerializer = new psbtSerializer(testcase.hex)
+    t.deepEqual(1, 1)
+  })
 })
